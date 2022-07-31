788775e0951a7f749886107cc4ed76151d45aac6
nachovilla123 <nacho.villarruel@gmail.com>
2022-07-31 18:13:10 -0300

    chau productos mios, prueba api

--- my-store/src/app/components/products/products.component.ts
+++ my-store/src/app/components/products/products.component.ts
@@ -16,32 +16,7 @@ export class ProductsComponent implements OnInit {
   total = 0;
 
 
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-  ];
+  products: Product[] = [];
 
   constructor(
     private storeService: StoreService // esto es una inyeccion de dependencias
--- my-store/src/app/services/products.service.ts
+++ my-store/src/app/services/products.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(
+    private http : HttpClient
+  ) {
+
+   }
 
   getAllProducts(){
-    
+    return this.http.get('https://fakestoreapi.com/products'); //http://fakestoreapi.com/docs
   }
 }
