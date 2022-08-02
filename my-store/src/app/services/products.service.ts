75fa9ce8d286eb0e1ee2da2417492a8209a0eee6
nachovilla123 <nacho.villarruel@gmail.com>
2022-08-02 17:52:17 -0300

    transformar peticiones :( no funciono

--- my-store/src/app/services/products.service.ts
+++ my-store/src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient , HttpParams,HttpErrorResponse,HttpStatusCode} from '@angular/common/http';
-import { retry, catchError } from 'rxjs/operators';
+import { retry, catchError ,map } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 import { UpdateProductDTO ,CreateProductDTO, Product } from './../models/product.model';
@@ -16,16 +16,21 @@ export class ProductsService {
     private http : HttpClient
   ) {}
 
-  getAllProducts(limit? : number,offset? : number){
+  getAllProducts(limit?: number, offset?: number) {
     let params = new HttpParams();
-    if(limit && offset){
-      params = params.set('limit',limit);
-      params = params.set('offset',offset);
+    if (limit && offset) {
+      params = params.set('limit', limit);
+      params = params.set('offset', limit);
     }
-
-    return this.http.get<Product[]>(this.apiUrl , {params})
+    return this.http.get<Product[]>(this.apiUrl, { params })
     .pipe(
-      retry(3) // reinento con observer
+      retry(3), // observer
+      map(products => products.map(item => {
+        return {
+          ...item,
+          taxes: .19 * item.price
+        }
+      }))
     );
   }
 
