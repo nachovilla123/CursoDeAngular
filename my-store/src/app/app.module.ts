80423649004de9e1a1813c70f3d98283bdaacb5d
nachovilla123 <nacho.villarruel@gmail.com>
2022-08-03 13:12:59 -0300

    fin

--- my-store/src/app/app.module.ts
+++ my-store/src/app/app.module.ts
@@ -1,47 +1,43 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SwiperModule } from 'swiper/angular';
 
-import { UsersService } from './services/users.service';
-import { FilesService } from './services/files.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
+import { TimeInterceptor } from './interceptors/time.interceptor';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImgComponent,
+    ProductComponent,
+    ProductsComponent,
+    NavComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    SwiperModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  imgParent = '';
-  showImg = true;
-  token = '';
-
-  constructor(
-    private usersService: UsersService,
-    private filesService: FilesService
-  ) {
-
-  }
-
-
-  onLoaded(img: string) {
-    console.log('log padre', img);
-  }
-
-  toggleImg() {
-    this.showImg = !this.showImg;
-  }
-
-  createUser() {
-    this.usersService.create({
-      name: 'Sebas',
-      email: 'sebas@mail.com',
-      password: '1212'
-    })
-    .subscribe(rta => {
-      console.log(rta);
-    });
-  }
-
-  downloadPdf() {
-    this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
-    .subscribe()
-  }
-}
+export class AppModule { }
